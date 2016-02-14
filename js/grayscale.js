fb6487048ec39785c53247c8a0ec06fa884a6857
Jan Štourač <honza.stourac@gmail.com>
2016-02-14 12:08:02 +0100

    Updated contact info

--- js/grayscale.js
+++ js/grayscale.js
@@ -38,129 +38,88 @@ function init() {
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
-        zoom: 15,
+        zoom: 17,
 
         // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(40.6700, -73.9400), // New York
+        center: new google.maps.LatLng(49.2011, 16.6434), // Gajdosova 18, Brno
 
         // Disables the default Google Maps UI components
         disableDefaultUI: true,
         scrollwheel: false,
         draggable: false,
 
-        // How you would like to style the map. 
+        // How you would like to style the map.
         // This is where you would paste any style found on Snazzy Maps.
         styles: [{
-            "featureType": "water",
-            "elementType": "geometry",
-            "stylers": [{
-                "color": "#000000"
+              "featureType": "landscape",
+              "stylers": [{
+                "hue": "#FFBB00"
+              }, {
+                "saturation": 43.400000000000006
+              }, {
+                "lightness": 37.599999999999994
+              }, {
+                "gamma": 1
+              }]
             }, {
-                "lightness": 17
+              "featureType": "road.highway",
+              "stylers": [{
+                "hue": "#FFC200"
+              }, {
+                "saturation": -61.8
+              }, {
+                "lightness": 45.599999999999994
+              }, {
+                "gamma": 1
+              }]
+            }, {
+              "featureType": "road.arterial",
+              "stylers": [{
+                "hue": "#FF0300"
+              }, {
+                "saturation": -100
+              }, {
+                "lightness": 51.19999999999999
+              }, {
+                "gamma": 1
+              }]
+            }, {
+              "featureType": "road.local",
+              "stylers": [{
+                "hue": "#FF0300"
+              }, {
+                "saturation": -100
+              }, {
+                "lightness": 52
+              }, {
+                "gamma": 1
+              }]
+            }, {
+              "featureType": "water",
+              "stylers": [{
+                "hue": "#0078FF"
+              }, {
+                "saturation": -13.200000000000003
+              }, {
+                "lightness": 2.4000000000000057
+              }, {
+                "gamma": 1
+              }]
+            }, {
+              "featureType": "poi",
+              "stylers": [{
+                "hue": "#00FF6A"
+              }, {
+                "saturation": -1.0989010989011234
+              }, {
+                "lightness": 11.200000000000017
+              }, {
+                "gamma": 1
+              }]
             }]
-        }, {
-            "featureType": "landscape",
-            "elementType": "geometry",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 20
-            }]
-        }, {
-            "featureType": "road.highway",
-            "elementType": "geometry.fill",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 17
-            }]
-        }, {
-            "featureType": "road.highway",
-            "elementType": "geometry.stroke",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 29
-            }, {
-                "weight": 0.2
-            }]
-        }, {
-            "featureType": "road.arterial",
-            "elementType": "geometry",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 18
-            }]
-        }, {
-            "featureType": "road.local",
-            "elementType": "geometry",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 16
-            }]
-        }, {
-            "featureType": "poi",
-            "elementType": "geometry",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 21
-            }]
-        }, {
-            "elementType": "labels.text.stroke",
-            "stylers": [{
-                "visibility": "on"
-            }, {
-                "color": "#000000"
-            }, {
-                "lightness": 16
-            }]
-        }, {
-            "elementType": "labels.text.fill",
-            "stylers": [{
-                "saturation": 36
-            }, {
-                "color": "#000000"
-            }, {
-                "lightness": 40
-            }]
-        }, {
-            "elementType": "labels.icon",
-            "stylers": [{
-                "visibility": "off"
-            }]
-        }, {
-            "featureType": "transit",
-            "elementType": "geometry",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 19
-            }]
-        }, {
-            "featureType": "administrative",
-            "elementType": "geometry.fill",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 20
-            }]
-        }, {
-            "featureType": "administrative",
-            "elementType": "geometry.stroke",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 17
-            }, {
-                "weight": 1.2
-            }]
-        }]
     };
 
-    // Get the HTML DOM element that will contain your map 
+    // Get the HTML DOM element that will contain your map
     // We are using a div with id="map" seen below in the <body>
     var mapElement = document.getElementById('map');
 
@@ -168,11 +127,11 @@ function init() {
     var map = new google.maps.Map(mapElement, mapOptions);
 
     // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
-    var image = 'img/map-marker.png';
-    var myLatLng = new google.maps.LatLng(40.6700, -73.9400);
+    //var image = 'img/map-marker.png';
+    var myLatLng = new google.maps.LatLng(49.2011, 16.6434);
     var beachMarker = new google.maps.Marker({
         position: myLatLng,
         map: map,
-        icon: image
+      //  icon: image
     });
 }
